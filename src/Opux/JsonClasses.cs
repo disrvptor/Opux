dfa6fb1fa105de017b1ae6ef57b5daa3c4a65045
Jimmy06 <robert@myhomedns.co.uk>
2017-12-20 20:13:05 +0000

    Final fixes for price checks

--- src/Opux/JsonClasses.cs
+++ src/Opux/JsonClasses.cs
@@ -52,11 +52,15 @@ namespace Opux
 
         public class AllianceData
         {
-            public string alliance_name { get; set; }
+            public string name { get; set; }
             public string ticker { get; set; }
+            public int creator_id { get; set; }
+            public int creator_corporation_id { get; set; }
+            public int executor_corporation_id { get; set; }
             public DateTime date_founded { get; set; }
-            public int executor_corp { get; set; }
+            public int faction_id { get; set; }
         }
+
 
         public class SystemIDSearch
         {
